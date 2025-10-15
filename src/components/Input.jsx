c7234d018a68209df531ace416089c04eddbb8ca
SirDeGuya <sirdeguya@gmail.com>
2025-10-15 18:33:49 +0300

    remove console log and add value attribute to the input component

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,7 +1,7 @@
-export default function Input({id, className, type="text", placeholder, onChange}) {
+export default function Input({id, className, type="text", placeholder, value, onChange}) {
      return (
          <div className={"w-full border-2 rounded-2xl px-4 py-2  my-6 " + className}>
-            <input className={`w-full`} id={id} type={type} placeholder={placeholder} onChange={onChange}/>
+            <input className={`w-full`} id={id} type={type} placeholder={placeholder} value={value} onChange={onChange}/>
         </div>
      )
 }
