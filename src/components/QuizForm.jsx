9510beab0b28e42b7061e980648ad5fcb36964d7
SirDeGuya <sirdeguya@gmail.com>
2025-10-19 20:46:56 +0300

    remove delete and question change from question component

--- src/components/QuizForm.jsx
+++ src/components/QuizForm.jsx
@@ -21,6 +21,17 @@ export default function QuizForm({ quiz, onSave }) {
         })
     }
 
+    function onQuestionChange(questionId, changeType, value) {
+        setQuestions(questions.map((questionItem) => {
+            return questionItem.id === questionId ? {...questionItem, [changeType]: value} : questionItem
+        }))
+    }
+
+    function onQuestionDelete(e, questionId) {
+        e.preventDefault()
+        setQuestions(questions.filter((question) => question.id !== questionId))
+    }
+
     function onSaveClick(e) {
         e.preventDefault()
         setIsLoading(true)
@@ -36,8 +47,7 @@ export default function QuizForm({ quiz, onSave }) {
                 <Input id={"title"} placeholder={"title"} value={title} onChange={(e) => setTitle(e.target.value)}/>
                 <div className={`flex gap-2 flex-col`}>
                     {questions.map((question,index, array) => {
-                        return <QuestionForm key={question.id} questionData={question} index={index} questions={array}
-                                             setQuestions={setQuestions}/>
+                        return <QuestionForm key={question.id} questionData={question} index={index} onQuestionChange={onQuestionChange} onQuestionDelete={onQuestionDelete}/>
                     })}
                 </div>
                 <RoundButton className={`bg-fuchsia-400 hover:bg-fuchsia-600`} onClick={onAddQuestion}>+</RoundButton>
@@ -50,32 +60,32 @@ export default function QuizForm({ quiz, onSave }) {
 }
 
 
-function QuestionForm({questionData, index, questions, setQuestions}) {
+function QuestionForm({questionData, index, onQuestionChange, onQuestionDelete}) {
 
-    function onQuestionChange(question, questionId) {
-        setQuestions(questions.map((questionItem) => {
-            console.log(questionItem)
-            return questionItem.id === questionId ? {...questionItem, question: question} : questionItem
-        }))
-    }
+    // function onQuestionChange(question, questionId) {
+    //     setQuestions(questions.map((questionItem) => {
+    //         console.log(questionItem)
+    //         return questionItem.id === questionId ? {...questionItem, question: question} : questionItem
+    //     }))
+    // }
+    //
+    // function onAnswerChange(answer, questionId) {
+    //     setQuestions(questions.map((questionItem) => {
+    //         return questionItem.id === questionId ? {...questionItem, answer: answer} : questionItem
+    //     }))
+    // }
 
-    function onAnswerChange(answer, questionId) {
-        setQuestions(questions.map((questionItem) => {
-            return questionItem.id === questionId ? {...questionItem, answer: answer} : questionItem
-        }))
-    }
-
-    function onQuestionDelete(e, questionId) {
-        e.preventDefault()
-        setQuestions(questions.filter((question) => question.id !== questionId))
-    }
+    // function onQuestionDelete(e, questionId) {
+    //     e.preventDefault()
+    //     setQuestions(questions.filter((question) => question.id !== questionId))
+    // }
 
 
     return (
         <div className={`border-2 rounded-2xl flex flex-row justify-between`}>
             <div className={`p-2 w-full`}>
-                <Input id="question" placeholder={`שאלה מספר ${index + 1}`} value={questionData.question} onChange={(e) => {onQuestionChange(e.target.value, questionData.id)}}/>
-                <Input id="answer" placeholder={`התשובה`} value={questionData.answer} onChange={(e) => {onAnswerChange(e.target.value, questionData.id)}}/>
+                <Input id="question" placeholder={`שאלה מספר ${index + 1}`} value={questionData.question} onChange={(e) => {onQuestionChange(questionData.id, 'question', e.target.value)}}/>
+                <Input id="answer" placeholder={`התשובה`} value={questionData.answer} onChange={(e) => {onQuestionChange(questionData.id, 'answer', e.target.value)}}/>
             </div>
             <div className={`p-2 flex flex-col justify-around border-s-2`}>
                 <RoundButton className={`bg-red-300 hover:bg-red-500`} onClick={(e) => onQuestionDelete(e, questionData.id)}>
