6a262312b053e9717665e43f074b8b289f798e98
SirDeGuya <sirdeguya@gmail.com>
2025-10-22 11:02:34 +0300

    make the navbar link reuseable

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -6,9 +6,59 @@ import {useAuth} from "../context/authContext.jsx";
 import { IoLogOutOutline } from "react-icons/io5";
 import {doSignOut} from "../lib/firebase/auth.js";
 import {Link} from "react-router";
+import {useEffect, useState} from "react";
 
 export default function NavBar() {
     const { userLoggedIn } = useAuth()
+
+    const navBarLinks = [
+        {
+            title: "בית",
+            link: "/",
+            style: `bg-indigo-400 hover:bg-indigo-600`,
+            icon: <IoIosHome className={`w-5 h-5`}/>,
+            onClick: undefined,
+            condition: true
+        },
+        {
+            title: "חדש",
+            link: "quiz/new",
+            style: `bg-amber-200 hover:bg-amber-400`,
+            icon: <CiCirclePlus className={`w-5 h-5`}/>,
+            onClick: undefined,
+            condition: userLoggedIn
+        },
+        {
+            title: "שאלונים שלי",
+            link: "/",
+            style: `bg-emerald-300 hover:bg-emerald-700`,
+            icon: <IoBookSharp className={`w-5 h-5`}/>,
+            onClick: undefined,
+            condition: userLoggedIn
+        },
+        {
+            title: "התנתקות",
+            link: undefined,
+            style: `bg-orange-300 hover:bg-orange-500`,
+            icon: <IoLogOutOutline className={`w-5 h-5`}/>,
+            onClick: doSignOut,
+            condition: userLoggedIn
+        },
+        {
+            title: "התחברות",
+            link: "/login",
+            style: `bg-lime-300 hover:bg-lime-600`,
+            icon: <IoLogInOutline className={`w-5 h-5`}/>,
+            onClick: undefined,
+            condition: !userLoggedIn
+        },
+    ]
+
+    const [ showLinksList, setShowLinksList ] = useState(navBarLinks)
+
+    useEffect(() => {
+        setShowLinksList([...navBarLinks.filter((link) => link.condition)])
+    }, [userLoggedIn]);
 
 
     return (
@@ -16,29 +66,14 @@ export default function NavBar() {
             <div className={`h-32`}></div>
             <div className={`w-full fixed bottom-0 start-0 p-4 bg-gray-100`}>
                 <div className={`flex flex-row justify-around`}>
-                    <Link to={`/`} className={`flex flex-col`}>
-                        <RoundButton className={`bg-indigo-400 hover:bg-indigo-600`}><IoIosHome className={`w-5 h-5`}/></RoundButton>
-                        בית
-                    </Link>
-                    {userLoggedIn && <>
-                        <Link to={`quiz/new`} className={`flex flex-col`}>
-                            <RoundButton className={`bg-amber-200 hover:bg-amber-400`}><CiCirclePlus className={`w-5 h-5`}/></RoundButton>
-                            חדש
-                        </Link>
-                        <Link to={`/`} className={`flex flex-col`}>
-                            <RoundButton className={`bg-emerald-300 hover:bg-emerald-700`}><IoBookSharp className={`w-5 h-5`}/></RoundButton>
-                            שאלונים שלי
-                        </Link>
-                    </>
-                    }
-                    {userLoggedIn ? <div className={`flex flex-col`}>
-                        <RoundButton className={`bg-orange-300 hover:bg-orange-500`} onClick={doSignOut}><IoLogOutOutline className={`w-5 h-5`}/></RoundButton>
-                        התנתקות
-                    </div> : <Link to={'/login'} className={`flex flex-col`}>
-                        <RoundButton className={`bg-lime-300 hover:bg-lime-600`}><IoLogInOutline className={`w-5 h-5`}/></RoundButton>
-                        התחברות
-                    </Link>
-                    }
+                    {showLinksList.map((link) => {
+                        return (
+                            <Link to={link.link} className={`flex flex-col`}>
+                                <RoundButton className={link.style} onClick={link.onClick}>{link.icon}</RoundButton>
+                                {link.title}
+                            </Link>
+                        )
+                    })}
                 </div>
             </div>
         </>
