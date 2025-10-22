07bd3efb4c2c0575a89b702c42521f015546a92f
SirDeGuya <sirdeguya@gmail.com>
2025-10-22 19:35:05 +0300

    add deploy config

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ function App() {
   return (
     <>
         <AuthProvider>
-            <BrowserRouter>
+            <BrowserRouter basename={import.meta.env.BASE_URL}>
                 <Routes>
                     <Route path='/' element={<Home/>}/>
                     <Route path={'/login'} element={<Login/>}/>
